db577916ae0b7020edde30fb34a9dd9e68411cc9
hvminh90 <hvminh90@gmail.com>
2015-12-30 00:15:34 -0800

    update 2

--- AngularJS_Test1/App_Start/RouteConfig.cs
+++ AngularJS_Test1/App_Start/RouteConfig.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Web.Http;
 using System.Web.Mvc;
 using System.Web.Routing;
 
@@ -12,12 +13,39 @@ namespace AngularJS_Test1
         public static void RegisterRoutes(RouteCollection routes)
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+            //routes.MapRoute(
+            //    name: "AngularCatchAllRoute",
+            //    url: "angular/{*.}",
+            //    defaults: new { controller = "Angular", action = "Index", id = UrlParameter.Optional }
+            //);
+
+            //routes.MapRoute(
+            //    name: "Default",
+            //    url: "{controller}/{action}/{id}",
+            //    defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+            //);
+            routes.MapRoute(
+                name: "about",
+                url: "Home/About",
+                defaults: new { controller = "Home", action = "About" });
 
             routes.MapRoute(
-                name: "Default",
-                url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
-            );
+                name: "NhanVien",
+                url: "Home/NhanVien",
+                defaults: new { controller = "Home", action = "NhanVien" });
+
+            routes.MapRoute(
+                name: "Info",
+                url: "Home/Info",
+                defaults: new { controller = "Home", action = "Info" });
+
+
+           routes.MapRoute(
+               name: "Default",
+               url: "{*url}",
+               defaults: new { controller = "Home", action = "Index" });
+
+            //routes.AppendTrailingSlash = true;
         }
     }
 }
--- AngularJS_Test1/Scripts/app.js
+++ AngularJS_Test1/Scripts/app.js
@@ -6,6 +6,14 @@ myApp.factory('AuthHttpResponseInterceptor', AuthHttpResponseInterceptor);
 myApp.factory('NhanVienFactory', NhanVienFactory);
 
 var configFunction = function ($routeProvider, $httpProvider, $locationProvider) {
+    //$locationProvider.html5Mode({
+    //    enabled: true,
+    //    requireBase: false
+    //}).hashPrefix('!');
+    var baseSiteUrlPath = $("base").first().attr("href");
+    var baseTemplateUrl = baseSiteUrlPath + "app/templates/";
+
+
     $routeProvider.
         when('/nhanVien', {
             templateUrl: 'Home/NhanVien',
@@ -28,10 +36,38 @@ var configFunction = function ($routeProvider, $httpProvider, $locationProvider)
         .otherwise({
             redirectTo: '/'
         });
+        //when('/angular/nhanvien', {
+        //    templateUrl: baseTemplateUrl + 'Index.html'
+        //})
+        ////.when('/routeTwo/:donuts', {
+        ////    templateUrl: function (params) { return '/routesDemo/two?donuts=' + params.donuts; }
+        ////})
+        //.when('/angular/home', {
+        //    templateUrl: 'Home/About',
+        //    controller: LoadPageCtr
+        //})
+        //.when('/', {
+        //    templateUrl: 'Home/Info',
+        //    controller: LoadPageCtr
+        //})
+        //.when('/nhanvien/add', {
+
+        //})
+        //.otherwise({
+        //    //redirectTo: '/'
+        //    redirectTo: function () {
+
+        //        if (window.location.pathname == baseSiteUrlPath || window.location.pathname == baseSiteUrlPath + "angular") {
+        //            window.location = baseSiteUrlPath + "angular/index";
+        //        } else {
+        //            window.location = baseSiteUrlPath + "angular/page-not-found";
+        //        }
+        //    },
+        //});
    
     $httpProvider.interceptors.push('AuthHttpResponseInterceptor');
     //use the HTML5 History API
-    //$locationProvider.html5Mode(true);
+    $locationProvider.html5Mode(true);
     //$locationProvider.html5Mode(false).hashPrefix('!');
 }
 configFunction.$inject = ['$routeProvider', '$httpProvider', '$locationProvider'];
